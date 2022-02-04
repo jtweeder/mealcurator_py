c2cb955f264d86a6390b3ef2d11953d3ed996921
jtweeder <31365056+jtweeder@users.noreply.github.com>
2022-02-03 22:50:48 -0600

    db connection and basic url digest

---
+++ mealcuratory_py/db_objs/db_conn.py
@@ -0,0 +1,52 @@
+import os
+import psycopg2
+import pandas as pd
+
+
+class postgre():
+
+    def __init__(self):
+        self.connection = psycopg2.connect(user=os.getenv('POSTGRES_USER'),
+                                           password=os.getenv('POSTGRES_PW'),
+                                           host=os.getenv('POSTGRESIP'),
+                                           port="5432",
+                                           database=os.getenv('POSTGRES_DB'))
+
+    def select(self, sql, idx=None):
+        """
+        Returns a SQL Query Result as a Dataframe
+
+        Parameters
+        ----------
+        sql : string
+            SQL query to execute
+        idx : str
+            Column name to use as an index
+
+        Returns
+        -------
+        select_df : DataFrame
+            Results from SQL query as DataFrame
+        """
+
+        cursor = self.connection.cursor()
+        cursor.execute(sql)
+        select_df = pd.DataFrame.from_records(cursor.fetchall(),
+                                              index=idx,
+                                              columns=[col[0]
+                                                       for col
+                                                       in cursor.description])
+        cursor.close()
+        return select_df
+
+    def insert(self):
+        # TODO
+        return
+
+    def update(self):
+        # TODO
+        return
+
+    def delete(self):
+        # TODO
+        return
